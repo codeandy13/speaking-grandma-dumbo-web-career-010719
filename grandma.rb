5e0bc057d1f1939a50481e38a95d6ea09c499895
Andrew Kim <akim618@gmail.com>
2018-12-17 00:55:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,13 @@
 def speak_to_grandma( "" )
   if "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY"
-  if "Hi!"
+  elsif condition
+     "Hi!"
     puts "HUH?! SPEAK UP, SONNY!"
-  if "I LOVE YOU GRANDMA!"
+  elsif "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  elsif "WHAT DID YOU EAT TODAY?"
+    puts "NO, NOT SINCE 1938!"
+  elsif "WHAT?"
+    puts "NO, NOT SINCE 1938!"
 end
